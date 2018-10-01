14c9f07fa170d66c0529b31d9e566a04577dcb31
Martin Stofko <martinstofko114@gmail.com>
2018-09-30 21:20:48 -0700

    added Cat class

---
+++ lib/cat.dart
@@ -0,0 +1,13 @@
+class Cat {
+  final String id;
+  final String url;
+
+  Cat({this.id, this.url});
+
+  factory Cat.fromJson(Map<String, dynamic> json) {
+    return Cat(
+      id: json['id'],
+      url: json['url'],
+    );
+  }
+}
