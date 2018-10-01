426f968a6fdf081c45f790a142ecb3d13ecd5a49
Martin Stofko <martinstofko114@gmail.com>
2018-09-30 20:51:15 -0700

    added background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+// import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final String _catApiUrl =
+      'https://api.thecatapi.com/v1/images/search?format=src&size=full';
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -12,8 +21,18 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.tealAccent,
       ),
       home: Scaffold(
-        body: Center(
-          child: Text('CatPix App'),
+        appBar: AppBar(
+          title: Text('CatPix'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Image.network(
+                _catApiUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
         ),
       ),
     );
